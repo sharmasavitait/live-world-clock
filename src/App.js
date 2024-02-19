de3f22dc360d8e36fad38579cb674886df5c0d65
sharmasavitait <sharmasavitait@gmail.com>
2024-02-19 17:49:35 +0530

    Base code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Clock from './Clock';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'></header>
+      <Clock />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Clock.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import TextTransition, { presets } from 'react-text-transition';
+import './Clock.css';
+
+export default function Clock() {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const hours = date.getHours();
+  const minuts = date.getMinutes();
+  const secs = date.getSeconds();
+
+  return (
+    <div className='clock_view'>
+      <TextTransition className='clock_text'>{hours}</TextTransition>
+      <p>:</p>
+      <TextTransition className='clock_text'>
+        {minuts < 10 ? `0${minuts}` : `${minuts}`}
+      </TextTransition>
+      <p>:</p>
+      <TextTransition className='clock_text'>
+        {secs < 10 ? `0${secs}` : `${secs}`}
+      </TextTransition>
+    </div>
+  );
+}
